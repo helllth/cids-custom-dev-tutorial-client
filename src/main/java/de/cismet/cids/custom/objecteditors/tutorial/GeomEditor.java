dbf6f8c27f7b17682f1c581dcc55dd17737b2a99
cidsCustomizer <cids.customizer@cismet.de>
2016-06-30 17:39:32 +0200

    working on #8: added a GeomEditor for automatically generated Editors

---
+++ src/main/java/de/cismet/cids/custom/objecteditors/tutorial/GeomEditor.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.cismet.cids.custom.objecteditors.tutorial;
+
+import de.cismet.cismap.cids.geometryeditor.DefaultCismapGeometryComboBoxEditor;
+
+/**
+ *
+ * @author thorsten
+ */
+public class GeomEditor extends DefaultCismapGeometryComboBoxEditor{
+    
+}
